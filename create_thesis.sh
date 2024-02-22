8cd7bd924fb77bc23216c08b1442a405eeb70a01
HANDZCZ <handz@email.cz>
2024-02-22 20:47:43 +0100

    Add linebreaks filter

--- create_thesis.sh
+++ create_thesis.sh
@@ -3,7 +3,7 @@
 echo 'Started conversion.'
 echo ''
 
-pandoc --standalone --listings --no-highlight --lua-filter ./filters/include-files.lua --lua-filter ./filters/include-code-files.lua --pdf-engine-opt=--shell-escape --pdf-engine-opt=--output-directory=temp --filter pandoc-plot --filter pandoc-crossref --lua-filter ./filters/add-missing-source.lua --lua-filter ./filters/minted.lua --lua-filter ./filters/pandoc-plot-fix-figures.lua --template template.latex --pdf-engine lualatex --bibliography bib.yaml --citeproc --csl iso690-numeric-brackets-cs.csl --lua-filter ./filters/remove-citation-brackets.lua -o 'thesis.pdf' main.md
+pandoc --standalone --listings --no-highlight --lua-filter ./filters/include-files.lua --lua-filter ./filters/include-code-files.lua --pdf-engine-opt=--shell-escape --pdf-engine-opt=--output-directory=temp --lua-filter ./filters/linebreaks.lua --filter pandoc-plot --filter pandoc-crossref --lua-filter ./filters/add-missing-source.lua --lua-filter ./filters/minted.lua --lua-filter ./filters/pandoc-plot-fix-figures.lua --template template.latex --pdf-engine lualatex --bibliography bib.yaml --citeproc --csl iso690-numeric-brackets-cs.csl --lua-filter ./filters/remove-citation-brackets.lua -o 'thesis.pdf' main.md
 
 mv temp/input.pdf ./thesis.pdf
 rm -r temp
